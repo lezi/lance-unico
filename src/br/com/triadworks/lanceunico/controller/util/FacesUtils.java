23d04defe6977365a8fa10d63b9a9b319fd9b753
Rafael Ponte <rponte@gmail.com>
2016-03-05 21:41:03 -0300

    refatora codigo;

--- src/br/com/triadworks/lanceunico/controller/util/FacesUtils.java
+++ src/br/com/triadworks/lanceunico/controller/util/FacesUtils.java
@@ -1,7 +1,11 @@
 package br.com.triadworks.lanceunico.controller.util;
 
+import static javax.faces.application.FacesMessage.SEVERITY_ERROR;
+import static javax.faces.application.FacesMessage.SEVERITY_INFO;
+
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
 
 @RequestScoped
@@ -17,14 +21,26 @@ public class FacesUtils {
 	 * Adiciona mensagem de sucesso na página
 	 */
 	public void info(String msg) {
-		facesContext.getExternalContext().getFlash().setKeepMessages(true);
-		facesContext.addMessage(null, infoMsg(msg));
+		keepMessagesAfterRedirect();
+		facesContext.addMessage(null, newMsg(msg, SEVERITY_INFO));
 	}
 
-	private FacesMessage infoMsg(String msg) {
+	/**
+	 * Adiciona mensagem de erro na página
+	 */
+	public void error(String msg) {
+		keepMessagesAfterRedirect();
+		facesContext.addMessage(null, newMsg(msg, SEVERITY_ERROR));
+	}
+
+	private FacesMessage newMsg(String msg, Severity severity) {
 		FacesMessage message = new FacesMessage(msg);
-		message.setSeverity(FacesMessage.SEVERITY_INFO);
+		message.setSeverity(severity);
 		return message;
+	}
+	
+	private void keepMessagesAfterRedirect() {
+		facesContext.getExternalContext().getFlash().setKeepMessages(true);
 	}
 
 }
