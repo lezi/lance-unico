0692cff4bd93ab5134f2950bcc6a27bee527ffba
Rafael Ponte <rponte@gmail.com>
2016-03-17 15:49:53 -0300

    TDD - cap04 - exercício com listas;

--- src/br/com/triadworks/lanceunico/service/Sorteio.java
+++ src/br/com/triadworks/lanceunico/service/Sorteio.java
@@ -1,5 +1,10 @@
 package br.com.triadworks.lanceunico.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.triadworks.lanceunico.modelo.Lance;
 import br.com.triadworks.lanceunico.modelo.Promocao;
 
@@ -7,6 +12,7 @@ public class Sorteio {
 	
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
+	private List<Lance> menores;
 	
 	public void sorteia(Promocao promocao) {
 		
@@ -19,8 +25,26 @@ public class Sorteio {
 				menorDeTodos = lance.getValor();
 			}
 		}
+		
+		encontraTresMenoresLancesNa(promocao);
+	}
+
+	private void encontraTresMenoresLancesNa(Promocao promocao) {
+		menores = new ArrayList<Lance>(promocao.getLances());
+        Collections.sort(menores, new Comparator<Lance>() {
+            public int compare(Lance o1, Lance o2) {
+                if(o1.getValor() < o2.getValor()) return -1;
+                if(o1.getValor() > o2.getValor()) return 1;
+                return 0;
+            }
+        });
+        menores = menores.subList(0, 3);
 	}
 	
+	public List<Lance> getTresMenoresLances() {
+		return menores;
+	}
+
 	public double getMaiorLance() {
 		return maiorDeTodos;
 	}
