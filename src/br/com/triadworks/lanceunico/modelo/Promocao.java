e8881d34b80a112067fa73cd15c09b2d5e8ca0bd
Rafael Ponte <rponte@gmail.com>
2016-03-29 21:27:23 -0300

    ops, pequena correção;

--- src/br/com/triadworks/lanceunico/modelo/Promocao.java
+++ src/br/com/triadworks/lanceunico/modelo/Promocao.java
@@ -105,7 +105,7 @@ public class Promocao implements Serializable {
 	 */
 	public void registra(Lance lance) {
 		
-		if (lance.getValor() < 0)
+		if (lance.getValor() <= 0)
 			throw new RuntimeException("Valor do lance deve ser maior que zero.");
 		
 		Cliente cliente = lance.getCliente();
