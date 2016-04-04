479d8686a7bb30fd1dfceaa79af8c27d7776a0d0
Rafael Ponte <rponte@gmail.com>
2016-04-04 17:32:57 -0300

    cap12, configurando selenium e conhecendo a API;

---
+++ test-aceitacao/cadastros/PesquisaNoGoogle.java
@@ -0,0 +1,27 @@
+package cadastros;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PesquisaNoGoogle {
+
+	public static void main(String[] args) {
+
+		// abre firefox
+		WebDriver driver = new FirefoxDriver();
+
+		// acessa o site do google
+		driver.get("http://www.google.com.br/");
+
+		// digita no campo com nome "q" do google
+		WebElement campoDeTexto = driver.findElement(By.name("q"));
+		campoDeTexto.sendKeys("triadworks");
+
+		// submete o form
+		campoDeTexto.submit();
+		
+		driver.close();
+	}
+}
