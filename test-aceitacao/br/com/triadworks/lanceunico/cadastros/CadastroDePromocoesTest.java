7d8f7fbe685029d857b30b880be7500b9f81467e
Rafael Ponte <rponte@gmail.com>
2016-04-06 11:44:31 -0300

    cap12, page objects de promocao;

--- test-aceitacao/br/com/triadworks/lanceunico/cadastros/CadastroDePromocoesTest.java
+++ test-aceitacao/br/com/triadworks/lanceunico/cadastros/CadastroDePromocoesTest.java
@@ -5,11 +5,10 @@ import static org.junit.Assert.assertTrue;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+
+import pageobjects.PromocoesPage;
 
 public class CadastroDePromocoesTest {
 	
@@ -28,28 +27,15 @@ public class CadastroDePromocoesTest {
 	@Test
 	public void deveAdicionarNovaPromocao() {
 		
-		driver.get("http://localhost:8080/lance-unico/pages/promocoes/novo.xhtml");
-
-        WebElement nome = driver.findElement(By.name("nome"));
-        nome.sendKeys("XBox One");
-        
-        WebElement valorMaximo = driver.findElement(By.name("valor"));
-        valorMaximo.clear(); // limpa input
-        valorMaximo.sendKeys("990,00");
-        
-        // checkbox
-        WebElement dinheiro = driver.findElement(By.name("dinheiro"));
-        dinheiro.click();
-        
-        // combobox
-        WebElement entrega = driver.findElement(By.name("entrega"));
-        Select combo = new Select(entrega);
-        combo.selectByVisibleText("Loja");
-
-        WebElement botao = driver.findElement(By.id("btn-salvar"));
-        botao.click();
-
-        WebElement mensagens = driver.findElement(By.id("mensagens"));
-        assertTrue(mensagens.getText().contains("Promoção criada com sucesso!"));
+		PromocoesPage listagem = new PromocoesPage(driver);
+		listagem.abre()
+			.novo()
+			.preencheNome("Xbox One")
+			.preencheValorMaximo("990,00")
+			.selecionaReceberEmDinheiro()
+			.selecionaLocalDeEntrega("Loja")
+			.submete();
+		
+        assertTrue(listagem.contemMensagem("Promoção criada com sucesso!"));
 	}
 }
